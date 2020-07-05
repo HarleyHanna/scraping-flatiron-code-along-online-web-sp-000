5c14cb5ad8d77fdac816fd74ac01e5010408ca9f
Harley Hanna <harleyhannawrit@gmail.com>
2020-07-05 19:46:13 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -28,7 +28,7 @@ class Scraper
     get_courses.each do |course|
       new_course = Course.new
       new_course.title = course.css("h2")
-      new_course.
+      new_course.schedule = course.css()
       
   end
   
